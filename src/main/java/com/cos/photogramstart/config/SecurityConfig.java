1a9be87c0fc755f7e8b0f9faf9b7b4596e1b3af7
jaejun1130 <wjswowns1234@gmail.com>
2021-06-30 00:12:41 +0900

    Security통한 url접근 설정

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity //해당 config 파일로 시큐리티를 활성
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http); 삭제시 기존 시큐리티가 가지고있는 설정이 초기화됨
+        http.authorizeRequests()
+            .antMatchers("/", "/user/**", "/image/**", "/subscribe/**","/comment/**").authenticated() //해당 url은 인증이 필요
+                .anyRequest().permitAll()   //그게 아닌 요청은 허용하겠다.
+                .and()                      //그리
+                .formLogin()                //로그인 페이지
+                .loginPage("/auth/signin")  //해당 url
+                .defaultSuccessUrl("/");    //성공하면 / url로
+    }
+}
