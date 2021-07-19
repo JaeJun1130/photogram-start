a6fda0ff89ee3ee42325e5991e9ab2acc84240e7
jaejun1130 <wjswowns1234@gmail.com>
2021-07-19 21:14:13 +0900

    Script로 화면ui

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.handler;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.utils.Script;
 import com.cos.photogramstart.web.dto.CMResDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,8 +14,9 @@ import java.util.Map;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
-    public CMResDto<?> validationsException(CustomValidationException e) {
-        return new CMResDto<Map<String,String>>(-1, e.getMessage(),e.getErrorMap());
+    public String validationsException(CustomValidationException e) {
+        return Script.back(e.getErrorMap().toString());
+//        return new CMResDto<Map<String,String>>(-1, e.getMessage(),e.getErrorMap());
     }
 
 }
